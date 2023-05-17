32efd03c93c57ebb36f53223d26a5f65d72a858f
Marc7Mxt <marcnasan@gmail.com>
2023-05-17 19:14:33 +0200

    -

--- TRABAJO_IPC/src/javafxmlapplication/JavaFXMLApplication.java
+++ TRABAJO_IPC/src/javafxmlapplication/JavaFXMLApplication.java
@@ -18,7 +18,7 @@ public class JavaFXMLApplication extends Application {
     public void start(Stage stage) throws Exception {
         //======================================================================
         // 1- creación del grafo de escena a partir del fichero FXML
-        FXMLLoader loader= new  FXMLLoader(getClass().getResource("/vistas/inicioalber.fxml"));
+        FXMLLoader loader= new  FXMLLoader(getClass().getResource("/vistas/iniciodefinitiva.fxml"));
         Parent root = loader.load();
         //======================================================================
         // 2- creación de la escena con el nodo raiz del grafo de escena
