b3ae35e4a8805581b8f3385e9d401ee479bc4d4f
Coryy82 <coryy822@gmail.com>
2023-05-23 11:27:23 +0200

    backup

---
+++ TRABAJO_IPC/src/controlador/MisReservasController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author coryy
+ */
+public class MisReservasController {
+
+    @FXML
+    private Button buttonAnular;
+
+    @FXML
+    private void anularHandle(ActionEvent event) {
+        
+    }
+    
+}
